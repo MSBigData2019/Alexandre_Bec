d8735e19baa2148b9bd7498d7b66d6ed2090e6e7
Alex <alx.bec@me.com>
2018-10-23 15:50:41 +0200

    Exo4

--- Lesson4-Cleaning/exo_dom_lesson04.py
+++ Lesson4-Cleaning/exo_dom_lesson04.py
@@ -1,1 +1,19 @@
-# Exo maison
+'''
+Générer un fichier de données sur le prix des Renault Zoé sur le marché de l'occasion en Ile de France, PACA et Aquitaine.
+Vous utiliserez lacentrale, paruvendu, autoplus,...
+Le fichier doit être propre et contenir les infos suivantes :
+    - version (il y en a 3),
+    - année, kilométrage,
+    - prix,
+    - téléphone du propriétaire,
+    - est ce que la voiture est vendue par un professionnel ou un particulier.
+
+    - ajouter une colonne sur le prix de l'Argus du modèle que vous récupérez sur ce site
+    http://www.lacentrale.fr/cote-voitures-renault-zoe--2013-.html.
+
+Les données quanti (prix, km notamment) devront être manipulables (pas de string, pas d'unité).
+Vous ajouterez une colonne si la voiture est plus chere ou moins chere que sa cote moyenne.﻿
+'''
+
+import requests
+
