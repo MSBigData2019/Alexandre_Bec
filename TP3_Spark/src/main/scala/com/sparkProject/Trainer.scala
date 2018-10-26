6a11f4002c0afecfc10ca47d67e9d6efd7c01637
Alex <alx.bec@me.com>
2018-10-26 11:48:37 +0200

    Spark TP3 Partie 2

--- TP3_Spark/src/main/scala/com/sparkProject/Trainer.scala
+++ TP3_Spark/src/main/scala/com/sparkProject/Trainer.scala
@@ -2,8 +2,13 @@ package com.sparkProject
 
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.ml
-import org.apache.spark.ml.feature.{CountVectorizer, IDF, RegexTokenizer, StopWordsRemover}
+import org.apache.spark.ml.feature._
+import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.evaluation.{MulticlassClassificationEvaluator, RegressionEvaluator}
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
+import org.apache.spark.mllib.evaluation.MulticlassMetrics
+import org.apache.spark.mllib.evaluation.{BinaryClassificationMetrics, MultilabelMetrics}
 
 
 object Trainer {
@@ -52,7 +57,9 @@ object Trainer {
     println(df.count())
 
 
-    /** TF-IF **/
+    /**
+      * Create Pipeline steps and classification model
+       */
 
     println("1. Tokenizer")
     // Split text into tokenized words
@@ -79,6 +86,97 @@ object Trainer {
     val idf = new IDF()
       .setInputCol("td")
       .setOutputCol("tfidf")
+
+    println("5,6. Convert categorical columns to indexed")
+    val index_country = new StringIndexer()
+      .setInputCol("country2")
+      .setOutputCol("country_indexed")
+      .setHandleInvalid("skip")
+
+
+    val index_currency = new StringIndexer()
+      .setInputCol("currency2")
+      .setOutputCol("currency_indexed")
+      .setHandleInvalid("skip")
+
+    println("7,8. OneHotEncoder")
+    val country_encoder = new OneHotEncoder()
+      .setInputCol("country_indexed")
+      .setOutputCol("countryVec")
+
+    val currency_encoder = new OneHotEncoder()
+      .setInputCol("currency_indexed")
+      .setOutputCol("currencyVec")
+
+
+    println("9. Assemble features into vector")
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("tfidf", "days_campaign", "hours_prepa", "goal", "countryVec", "currencyVec"))
+      .setOutputCol("features")
+
+    println("10. Declare Classification Model")
+    val model_classifier = new LogisticRegression()
+      .setElasticNetParam(0.0)
+      .setFitIntercept(true)
+      .setFeaturesCol("features")
+      .setLabelCol("final_status")
+      .setStandardization(true)
+      .setPredictionCol("predictions")
+      .setRawPredictionCol("raw_predictions")
+      .setThresholds(Array(0.7, 0.3))
+      .setTol(1.0e-6)
+      .setMaxIter(300)
+
+    /**
+      * Pipeline and Training
+      */
+    println("11. Pipeline de la win")
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer,
+        swremover, countvect, idf,
+        index_country, country_encoder, index_currency,currency_encoder,
+        assembler, model_classifier))
+
+    val Array(training, test) = df.randomSplit(Array(0.9, 0.1), seed = 12345)
+
+    pipeline.fit(training)
+
+    /**
+      * Model parameters with grid-search
+      * - Create a grid with parameters values
+      * - Use a validation Set to evaluate parameters
+      * - Then keep parameters where validation error is minimal
+      */
+    println("12. Model Parameters estimation")
+
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(model_classifier.regParam, Array(10e-8, 10e-6, 10e-4, 10e-2))
+      .addGrid(countvect.minDF, Array(55.0, 75.0, 95.0))
+      .build()
+
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("final_status")
+      .setPredictionCol("predictions")
+
+    val trainvalidation = new TrainValidationSplit()
+      .setEstimator(pipeline)
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      // 70% of the data will be used for training and the remaining 30% for validation.
+      .setTrainRatio(0.7)
+
+    val validation_model = trainvalidation.fit(training)
+
+    /**
+      * Make predictions on the test-dataset
+    **/
+
+    val df_predictions = validation_model
+      .transform(test)
+      .select("features", "final_status", "predictions", "raw_predictions")
+
+    val metrics = evaluator.evaluate(df_predictions)
+    println("f1score : "+ metrics)
 
   }
 }
